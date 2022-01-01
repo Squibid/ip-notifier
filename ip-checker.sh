b3e359b81219f26facb39819666f35e7e2c3282a
Squibid <71739600+Squibid@users.noreply.github.com>
2022-01-01 17:58:20 +0000

    Update ip-checker.sh

--- ip-checker.sh
+++ ip-checker.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 currentIp=$(curl -s http://whatismyip.akamai.com/)
-timeStamp=$(date +"[%Y/%m/%d %H:%M:%S]")
 configFile=`cat config.txt`
 
 set -o noclobber
-echo "$timeStamp Script started." >> ip-dumps.txt
-echo $timeStamp $currentIp >> ip-dumps.txt
+echo "$(date +"[%Y/%m/%d %H:%M:%S]") Script started." >> ip-dumps.txt
+echo $(date +"[%Y/%m/%d %H:%M:%S]") $currentIp >> ip-dumps.txt
 
 while [ currentIp != " " ]
 do
-nextIp=$(curl -s http://whatismyip.akamai.com)
-if [ $nextIp != $currentip ]; then
-        echo "$timeStamp New ip: $nextIp" >> ip-dumps.txt
+nextIp=$(curl -s http://whatismyip.akamai.com/)
+if [ $nextIp != $currentIp ]; then
+        echo "$(date +"[%Y/%m/%d %H:%M:%S]") New ip: $nextIp" >> ip-dumps.txt
         currentIp=$nextIp
 else
         echo "same ip as before"
         sleep $(echo $configFile | awk '{print $7}')
 fi
 done
-
-if [ currentIp = " " ]; then
-        echo "$timestamp !!Not connected to a network!!" >> ip-dumps.txt
