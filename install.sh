3436e3b610efce1629af983d0abd861746cdb699
Squibid <71739600+Squibid@users.noreply.github.com>
2022-01-02 09:24:32 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+chmod +x main.sh
 chmod +x functions/ip-checker.sh
 
 
-
-echo Fully installed if anything went wrong please open an issue at https://github.com/Squibid/ip-notifier/issues
+read -p "Installation complete would you like to run the script [y],[n]? " runScript
+if [ "$runScript" ==  "y" ]; then
+        echo Running script
+        ./main.sh
+else
+        echo You can manually start the script by running bash main.sh
+fi
