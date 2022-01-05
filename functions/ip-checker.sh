924699d3dde18f69e4eb1801952d1da9a05ed1de
Squibid <71739600+Squibid@users.noreply.github.com>
2022-01-05 00:43:48 +0000

    Update ip-checker.sh

--- functions/ip-checker.sh
+++ functions/ip-checker.sh
@@ -2,20 +2,20 @@
 currentIp=$(curl -s http://whatismyip.akamai.com/)
 configFile=`cat CONFIG`
 ipLogFile="$(echo $configFile | awk '{print $8}')"
-
+ 
 set -o noclobber
 echo "$(date +"[%Y/%m/%d %H:%M:%S]") Script started" >> $ipLogFile
-echo $(date +"[%Y/%m/%d %H:%M:%S]") $currentIp >> $ipLogFile
+echo "$(date +"[%Y/%m/%d %H:%M:%S]") $currentIp" >> $ipLogFile
 
 while [ currentIp != " " ]
 do
 nextIp=$(curl -s http://whatismyip.akamai.com/)
 if [ "$nextIp" != $currentIp ]; then
-        echo "$(date +"[%Y/%m/%d %H:%M:%S]") New ip: $nextIp" >> $ipLogFile
-        currentIp=$nextIp
-        echo "New ip $currentIp"
+	echo "$(date +"[%Y/%m/%d %H:%M:%S]") New ip: $nextIp" >> $ipLogFile
+	currentIp=$nextIp
+	./ftp.sh
 else
-        echo "same ip as before"
-        sleep $(echo $configFile | awk '{print $4}')
+	echo "same ip as before"
+	sleep $(echo $configFile | awk '{print $4}')
 fi
 done
