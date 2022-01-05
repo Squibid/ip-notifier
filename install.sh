ffc69e00075107809271b8c8e48a5cef197ef129
Squibid <71739600+Squibid@users.noreply.github.com>
2022-01-05 00:42:15 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 chmod +x main.sh
 chmod +x functions/ip-checker.sh
-
+chmod +x functions/ftp.sh
 
 read -p "Installation complete would you like to run the script [y],[n]? " runScript
 if [ "$runScript" ==  "y" ]; then
         echo Running script
         ./main.sh
 else
-        echo You can manually start the program by running bash main.sh or ./main.sh
+        echo You can manually start the script by running bash main.sh
 fi
