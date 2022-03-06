bec57d604b429f9dbd40e927731b46cbbb650e03
Squibid <71739600+Squibid@users.noreply.github.com>
2022-03-06 18:22:44 -0500

    Update ip-checker.sh

--- functions/ip-checker.sh
+++ functions/ip-checker.sh
@@ -2,20 +2,23 @@
 configFile=`cat CONFIG`
 currentIp=$(curl -s http://whatismyip.akamai.com/)
 ipLogFile="$(echo $configFile | awk '{print $8}')"
- 
+ipNotMet="$(echo $configFile | awk '{print $12}')"
+
 set -o noclobber
 echo "$(date +"[%Y/%m/%d %H:%M:%S]") Script started" >> $ipLogFile
 echo "$(date +"[%Y/%m/%d %H:%M:%S]") $currentIp" >> $ipLogFile
 
-while [ currentIp != " " ]
-do
-nextIp=$(curl -s http://whatismyip.akamai.com/)
-if [ "$nextIp" != $currentIp ]; then
-	echo "$(date +"[%Y/%m/%d %H:%M:%S]") New ip: $nextIp" >> $ipLogFile
-	currentIp=$nextIp
-	./ftp.sh
-else
-	echo "same ip as before"
-	sleep $(echo $configFile | awk '{print $4}')
-fi
+while [ currentIp != " " ]; do
+    nextIp="beans"
+    if [ "$nextIp" != $currentIp ]; then 
+        echo "$(date +"[%Y/%m/%d %H:%M:%S]") New ip: $nextIp" >> $ipLogFile
+        currentIp=$nextIp
+        if [ "$ipNotMet" == "ftp" ]; then
+            python ip-ftp.py
+        if [ "$ipNotMet" == "email" ]; then
+            python ip-email.py
+    else
+		echo "same ip as before"
+        sleep "$(echo $configFile | awk '{print $4}')"
+    fi
 done
