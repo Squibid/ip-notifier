8812c642df89cdd82be028bde42088fee82fedf6
Squibid <71739600+Squibid@users.noreply.github.com>
2022-03-07 16:40:31 -0500

    The fix worked \o/

--- functions/ip-checker.sh
+++ functions/ip-checker.sh
@@ -13,7 +13,7 @@ while [ currentIp != " " ]; do
     if [ "$nextIp" != $currentIp ]; then 
         echo "$(date +"[%Y/%m/%d %H:%M:%S]") New ip: $nextIp" >> $ipLogFile
         currentIp=$nextIp
-        python 'ip-'$ipNotMet'.py'
+        python functions/'ip-'$ipNotMet'.py'
     else
 		echo "same ip as before"
         sleep "$(echo $configFile | awk '{print $4}')"
